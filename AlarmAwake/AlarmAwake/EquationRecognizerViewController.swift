e600841e716c1f1c9c7afd1874274dd56471e720
nicholec <niclarke7@gmail.com>
2018-04-07 00:21:27 -0400

    Add working processing

--- AlarmAwake/AlarmAwake/EquationRecognizerViewController.swift
+++ AlarmAwake/AlarmAwake/EquationRecognizerViewController.swift
@@ -2,21 +2,25 @@
 //  EquationRecognizerViewController.swift
 //  AlarmAwake
 //
-//  Created by mac on 4/5/18.
+//  Created by Nichole Clarke on 4/5/18.
 //  Copyright © 2018 mac. All rights reserved.
 //
 
 import UIKit
 import AVFoundation
 import Speech
+import SwiftTryCatch
 
-class EquationRecognizerViewController: UIViewController {
+class EquationRecognizerViewController: UIViewController, SFSpeechRecognitionTaskDelegate {
     
     fileprivate var player: AVPlayer?
     let audioEngine = AVAudioEngine()
     let speechRecognizer = SFSpeechRecognizer()
-    let request = SFSpeechAudioBufferRecognitionRequest()
+    var request = SFSpeechAudioBufferRecognitionRequest()
     var recognitionTask: SFSpeechRecognitionTask?
+    var equation: String = ""
+    let synth = AVSpeechSynthesizer()
+    let answer = arc4random_uniform(109)
     
     @IBOutlet weak var recordingButton: UIButton!
     @IBOutlet weak var numberLabel: UILabel!
@@ -27,32 +31,34 @@ class EquationRecognizerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         requestAuthorization()
+        self.numberLabel.text = String(answer)
+        self.equationLabel.text = ""
         
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress(_:)))
+        longPressGesture.cancelsTouchesInView = false
         recordingButton.addGestureRecognizer(longPressGesture)
     }
     
     @IBAction func longPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
-        print("We made it here.")
         switch  gestureRecognizer.state {
         case .began:
+            print("BEGAN")
             startRecording()
         case .ended:
+            print("exited")
             stopRecording()
+            processEquation()
         default:
             break
         }
-    }
-    
-    func longPress() {
-        
     }
     
     private func requestAuthorization() {
         SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
             switch authStatus {
             case .authorized:
-                self.startRecording()
+                break
+//                self.startRecording()
             case .denied:
                 print("Denied")
             // show an alert instead
@@ -66,31 +72,81 @@ class EquationRecognizerViewController: UIViewController {
     }
     
     private func startRecording() {
-        self.equationLabel.text = ""
+        DispatchQueue.main.async {
+            self.equation = ""
+            self.equationLabel.text = "Your equation:"
+        }
+        
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
         
         let node = audioEngine.inputNode
-        let recordingForm = node.outputFormat(forBus: 0)
+        let recordingFormat = node.outputFormat(forBus: 0)
         
-        node.installTap(onBus: 0, bufferSize: 1024, format: recordingForm) { [unowned self] buffer, _ in
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { [unowned self] buffer, _ in
             self.request.append(buffer)
         }
         
         audioEngine.prepare()
         do {
             try audioEngine.start()
-            recognitionTask = speechRecognizer?.recognitionTask(with: request) { [unowned self] (result, _) in
-                if let transcription = result?.bestTranscription {
-                    self.equationLabel.text = "Your equation: \(transcription.formattedString)"
-                }
-            }
+            recognitionTask = speechRecognizer?.recognitionTask(with: request, delegate: self)
         } catch (let error) {
             print("There was a problem starting the recording: \(error.localizedDescription)")
         }
     }
     
+    internal func speechRecognitionTask(_ task: SFSpeechRecognitionTask, didHypothesizeTranscription transcription: SFTranscription) {
+        equation = transcription.formattedString.replace(target: " one", withString: "1")
+        self.equationLabel.text = "Your equation: \(equation)"
+    }
+    
     private func stopRecording() {
         audioEngine.stop()
+        audioEngine.inputNode.removeTap(onBus: 0)
         request.endAudio()
         recognitionTask?.cancel()
+        request = SFSpeechAudioBufferRecognitionRequest()
+    }
+    
+    private func processEquation() {
+        var utterance = AVSpeechUtterance(string: "")
+        utterance.rate = 0.5
+        if equation.isEmpty {
+            utterance = AVSpeechUtterance(string: "Make sure you say an equation")
+            self.synth.speak(utterance)
+        } else {
+            SwiftTryCatch.try({
+                let expr = NSExpression(format: self.equation)
+                if let result = expr.expressionValue(with: [], context: nil) as? Double {
+                    print(result)
+                    let response = "\(self.equation) equals \(Int(result)). \(Double(self.answer) == result ? "That's right!" : "Try again.")"
+                    utterance = AVSpeechUtterance(string: response)
+                    self.synth.speak(utterance)
+                } else {
+                    utterance = AVSpeechUtterance(string: "Unable to process your equation: \(self.equation)")
+                    self.synth.speak(utterance)
+                }
+            }, catch: { (error) in
+                utterance = AVSpeechUtterance(string: "Unable to process your equation: \(self.equation)")
+                self.synth.speak(utterance)
+            }, finallyBlock: {
+                // close resources
+            })
+        }
     }
 }
+
+extension String {
+    func matches(_ regex: String) -> Bool {
+        return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+    
+    func replace(target: String, withString: String) -> String {
+        return self.replacingOccurrences(of: target, with: withString, options: NSString.CompareOptions.literal, range: nil)
+    }
+}
+
+
